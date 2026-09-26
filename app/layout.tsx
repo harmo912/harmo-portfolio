dd3a5c61a116992dbde4636fcc3ad92919072533
harmo <harmohlb01@gmail.com>
2026-09-26 18:30:49 +0100

    fix typescript children type

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import type { ReactNode } from 'react'
 import './globals.css'
 
 export const metadata = {
@@ -9,7 +9,7 @@ export const metadata = {
   },
 }
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="fr">
       <head>
